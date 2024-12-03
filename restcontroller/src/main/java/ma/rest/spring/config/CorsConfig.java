2a990053f6bb26b76db73853eca9579f145c817e
FouziLoubna <fouziloubna031@gmail.com>
2024-12-03 20:24:34 +0100

    restcontroller
    restcontroller

---
+++ restcontroller/src/main/java/ma/rest/spring/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package ma.rest.spring.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Autoriser toutes les routes
+                        .allowedOrigins("http://localhost:3002") // Frontend React.js
+                        .allowedMethods("GET", "POST") // Méthodes HTTP
+                        .allowedHeaders("*"); // Tous les en-têtes
+            }
+        };
+    }
+}
